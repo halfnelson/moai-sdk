ff9fe172ef7c8649219a1ccb6ddbf9022ee870d5
David Pershouse <dpershouse@gmail.com>
2014-06-04 23:09:42 +1000

    start on ios host project

--- bin/build-ios-host.sh
+++ bin/build-ios-host.sh
@@ -9,9 +9,9 @@ APP_VERSION='1.0'
 
 # check for command line switches
 usage="usage: $0  \
-    [--use-untz true | false] [--disable-adcolony] [--disable-billing] \
+    [--disable-adcolony] [--disable-billing] \
     [--disable-chartboost] [--disable-crittercism] [--disable-facebook] [--disable-push] [--disable-tapjoy] \
-    [--disable-twitter]  <lua_src_directory>"
+    [--disable-twitter]  <lib_directory> <host_directory> <lua_src_directory>"
 
 use_untz="true"
 
@@ -24,12 +24,10 @@ push_flags=
 tapjoy_flags=
 twitter_flags=
 buildtype_flags="Debug"
-windows_flags=
-simulator="false"
 
-while [ $# -gt 1 ];	do
+
+while [ $# -gt 3 ];	do
     case "$1" in
-        --use-untz)  use_untz="$2"; shift;;
         --disable-adcolony)  adcolony_flags="-DDISABLE_ADCOLONY";;
         --disable-billing)  billing_flags="-DDISABLE_BILLING";;
         --disable-chartboost)  chartboost_flags="-DDISABLE_CHARTBOOST";;
@@ -38,7 +36,6 @@ while [ $# -gt 1 ];	do
         --disable-push)  push_flags="-DDISABLE_NOTIFICATIONS";;
         --disable-tapjoy)  tapjoy_flags="-DDISABLE_TAPJOY";;
         --disable-twitter)  twitter_flags="-DDISABLE_TWITTER";;
-        
         -*)
             echo >&2 \
                 $usage
@@ -48,20 +45,19 @@ while [ $# -gt 1 ];	do
     shift
 done
 
+LIB_DIR=$1
+HOST_DIR=$2
+
 SRCPARAM='./samples/hello-moai'
-if [ x != x"$1" ]; then
-   SRCPARAM=$1 
+if [ x != x"$3" ]; then
+   SRCPARAM=$3 
 fi
+
 LUASRC=$(ruby -e 'puts File.expand_path(ARGV.first)' "$SRCPARAM")
 
 if [ ! -f "${LUASRC}/main.lua" ]; then
   echo "Could not find main.lua in specified lua source directory [${LUASRC}]"
   exit 1
-fi
-
-if [ x"$use_untz" != xtrue ] && [ x"$use_untz" != xfalse ]; then
-    echo $usage
-    exit 1		
 fi
 
 #get some required variables
@@ -74,13 +70,6 @@ echo "Building moai.app via CMAKE"
 
 disabled_ext=
     
-if [ x"$use_untz" != xtrue ]; then
-    echo "UNTZ will be disabled"
-    untz_param='-DMOAI_UNTZ=0'
-else
-    untz_param='-DMOAI_UNTZ=1'
-fi 
-
 if [ x"$adcolony_flags" != x ]; then
     echo "AdColony will be disabled"
     disabled_ext="$disabled_extADCOLONY;"
@@ -123,24 +112,20 @@ fi
 
 build_dir=${PWD}
 
- cd cmake
- mkdir projects
- cd projects
- rm -rf moai-ios
- mkdir moai-ios-host
- cd moai-ios-host
+ mkdir -p $HOST_DIR/src
+
+ cd $HOST_DIR
+
+ cp -R ${build_dir}/src/host-ios .
+ cp -R ${build_dir}/src/moai-iphone .
  
  echo "Building resource list from ${LUASRC}"
- ruby ../../host-ios/build_resources.rb "${LUASRC}"
+ ruby ${build_dir}/cmake/host-ios/build_resources.rb "${LUASRC}"
 
  echo "Creating xcode project"
 
 #create our makefiles
-cmake -DDISABLED_EXT="$disabled_ext" -DMOAI_BOX2D=1 \
--DMOAI_CHIPMUNK=1 -DMOAI_CURL=1 -DMOAI_CRYPTO=1 -DMOAI_EXPAT=1 -DMOAI_FREETYPE=1 \
--DMOAI_HTTP_CLIENT=1 -DMOAI_JSON=1 -DMOAI_JPG=1 -DMOAI_LUAEXT=1 \
--DMOAI_MONGOOSE=1 -DMOAI_OGG=1 -DMOAI_OPENSSL=1 -DMOAI_SQLITE3=1 \
--DMOAI_TINYXML=1 -DMOAI_PNG=1 -DMOAI_SFMT=1 -DMOAI_VORBIS=1 $untz_param \
+cmake -DDISABLED_EXT="$disabled_ext" 
 -DBUILD_IOS=true \
 -DSIGN_IDENTITY="${SIGN_IDENTITY}" \
 -DAPP_NAME="${APP_NAME}" \
@@ -149,6 +134,7 @@ cmake -DDISABLED_EXT="$disabled_ext" -DMOAI_BOX2D=1 \
 -DCMAKE_BUILD_TYPE=$buildtype_flags \
 -DHOST_ONLY=True \
 -DCMAKE_SUPPRESS_REGENERATION=True \
--DLIB_PATH=${build_dir}/lib/ios \
+-DLIB_PATH=${LIB_DIR} \
+-DHOST_PATH=${HOST_DIR}/src
 -G "Xcode" \
-../../host-ios
+${build_dir}/cmake/host-ios
